1446e0288c95fd04cf24a86c7737e0bfbbf47ff6
Nattiden <2682645990@qq.com>
2025-06-01 16:43:00 +0800

    base64 hex

--- src/baconencoding/encoding_parsers/base64.py
+++ src/baconencoding/encoding_parsers/base64.py
@@ -1,14 +1,14 @@
 import base64
 from ..utils.base import EncodingBase
 
-UPPER_A_ASCII = ord('A')
-
 class Base64Encode(EncodingBase):
     name = 'base64'
     command_help = 'base64'
 
     def encode(self,code:str,_parser_args,*_args,**_kwargs) -> str:
         encoder = base64.b64encode(code.encode('utf-8'))
+        if _parser_args.get('hex'):
+            return encoder.hex()
         return encoder.decode('utf-8')
 
     def decode(self,code:str,_parser_args,*_args,**_kwargs) -> str:
