495d456cdc98892bdaf87395d503715bee36dfe0
Nattiden <2682645990@qq.com>
2025-06-01 16:43:13 +0800

    vigenere shift

--- src/baconencoding/encoding_parsers/vigenere.py
+++ src/baconencoding/encoding_parsers/vigenere.py
@@ -4,29 +4,30 @@ from .caesar import caesar
 UPPER_A_ASCII = ord('A')
 LOWER_A_ASCII = ord('a')
 
-def getkeylist(key:str):
-    return [ord(keychar) - UPPER_A_ASCII for keychar in key.upper()]
+def getkeylist(key:str, shift_value:int = 0) -> list[int]:
+    return [ord(keychar) - UPPER_A_ASCII + shift_value for keychar in key.upper()]
 
 class Vigenere(EncodingBase):
     name = 'vigenere'
     command_help = 'Parse Vigenere'
 
     def encode(self,code:str,parser_args,*args,**kwargs) -> str:
-        keylist = getkeylist(parser_args.key)
+        keylist = getkeylist(parser_args.key, shift_value=parser_args.shift)
         result = ''
-        for i in range(len(code)):
-            result += caesar(code = code[i], shift = keylist[i%len(keylist)])
+        for i, chara in enumerate(code):
+            result += caesar(code = chara, shift = keylist[i%len(keylist)])
         return result
 
     def decode(self,code:str,parser_args,*args,**kwargs) -> str:
-        keylist = getkeylist(parser_args.key)
+        keylist = getkeylist(parser_args.key, shift_value=-parser_args.shift)
         result = ''
-        for i in range(len(code)):
-            result += caesar(code = code[i], shift = -keylist[i%len(keylist)])
+        for i, chara in enumerate(code):
+            result += caesar(code = chara, shift = -keylist[i%len(keylist)])
         return result
 
     def __init__(self):
         super().__init__()
         self.parser.add_argument('-k','--key',help='The key of the cipher')
+        self.parser.add_argument('-s','--shift',help='Shift value', type=int, default=0)
 
 Vigenere()
